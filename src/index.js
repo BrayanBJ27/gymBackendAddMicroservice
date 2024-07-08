4ce85b21482c2e508faf7a4d1d5e53b6fdbcce66
BrayanBJ27 <xavi_braian@hotmail.com>
2024-07-07 20:23:00 -0500

    feat: :bug: Add route to add a new reserve

--- src/index.js
+++ src/index.js
@@ -1,10 +1,8 @@
 const express = require('express');
 const mysql = require('mysql2');
 const cors = require('cors');
-
 const app = express();
-const port = 8088
-;
+const port = process.env.Por || 8088;
 
 // Middleware
 app.use(cors());
@@ -12,10 +10,10 @@ app.use(express.json());
 
 // Configure the database connection
 const db = mysql.createConnection({
-    host: 'mysql-programaciondis.alwaysdata.net',
-    user: '357676_bj',
-    password: 'Uyt:tBHLgt4Kk_E',
-    database: 'programaciondis_gym'
+    host: process.env.DB_HOST,
+    user: process.env.DB_USER,
+    password: process.env.DB_PASSWORD,
+    database: process.env.DB_NAME
 });
 
 db.connect((err) => {
@@ -26,9 +24,19 @@ db.connect((err) => {
     console.log('Connected to the MySQL database.');
 });
 
-// Import routes
-const machineReservationsRoutes = require('./routes/machineReservations')(db);
-app.use('/reservations', machineReservationsRoutes);
+// Route to add a new reserve
+app.post('/reservations', (req, res) => {
+    const { machineName, userName, startTime, endTime } = req.body;
+    const insertQuery = 'INSERT INTO machine_reservations (machine_name, user_name, start_time, end_time) VALUES (?, ?, ?, ?)';
+    db.query(query, [machineName, userName, startTime, endTime], (err, result) => {
+        if (err) {
+            console.error('Error adding reservation:', err);
+            res.status(500).send({ message: 'Error adding reservation', error: err });
+            return;
+        }
+        res.status(201).send({ id: result.insertId, machineName, userName, startTime, endTime });
+    });
+});
 
 // Root route to check if the server is running
 app.get('/', (req, res) => {
@@ -36,5 +44,5 @@ app.get('/', (req, res) => {
 });
 
 app.listen(port, () => {
-    console.log(`Machines service listening at http://localhost:${port}`);
+    console.log(`Add Reservations Machines service listening at http://localhost:${port}`);
 });
